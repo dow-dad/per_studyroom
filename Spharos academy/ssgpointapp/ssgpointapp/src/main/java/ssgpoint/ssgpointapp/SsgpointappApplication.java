57fabeaf65c3f8b3afb7e46afad36d5a5a7ec541
dow-dad <sojunyeong301@gmail.com>
2023-08-12 23:53:16 +0900

    tailwond,next.js 추가

---
+++ Spharos academy/ssgpointapp/ssgpointapp/src/main/java/ssgpoint/ssgpointapp/SsgpointappApplication.java
@@ -0,0 +1,13 @@
+package ssgpoint.ssgpointapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgpointappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsgpointappApplication.class, args);
+	}
+
+}
---
+++ Spharos academy/ssgpointapp/ssgpointapp/src/test/java/ssgpoint/ssgpointapp/SsgpointappApplicationTests.java
@@ -0,0 +1,13 @@
+package ssgpoint.ssgpointapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgpointappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
